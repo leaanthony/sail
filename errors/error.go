0cbfbaabdcfb51152c035e86fc4cddd5651aee0c
Lea Anthony <lea.anthony@gmail.com>
2019-05-02 21:47:53 +1000

    update Error names

--- errors/error.go
+++ errors/error.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 )
 
-// SErrorStruct is like an error but just holds error types
-type SErrorStruct struct {
+// SError is like an error but just holds error types
+type SError struct {
 	Type int
 	Message string
 }
 
 // Error returns a string representing the error
-func (s *SErrorStruct) Error() string {
+func (s *SError) Error() string {
 	message := "Error[%d]"
 	if s.Message != "" {
 		message += ": %s"
@@ -19,9 +19,9 @@ func (s *SErrorStruct) Error() string {
 	return fmt.Sprintf(message, s.Type, s.Message)
 }
 
-// SError returns a new Error of the given type
-func SError(Type int, message ...string) *SErrorStruct {
-	result := &SErrorStruct{
+// NewError returns a new Error of the given type
+func NewError(Type int, message ...string) *SError {
+	result := &SError{
 		Type: Type,
 	}
 	if len(message) > 0 {
