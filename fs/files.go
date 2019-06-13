1fe731f302fa657dfbdbd0525c6728a2247ab523
Lea Anthony <lea.anthony@gmail.com>
2019-06-13 18:29:51 +1000

    feat: add DirExists()

--- fs/files.go
+++ fs/files.go
@@ -7,3 +7,12 @@ func FileExists(file string) bool {
 	_, err := os.Stat(file)
 	return err == nil
 }
+
+// DirExists returns true if the given file exists and is a directory
+func DirExists(dir string) bool {
+	stat, err := os.Stat(dir)
+	if err != nil {
+		return false
+	}
+	return stat.IsDir()
+}
